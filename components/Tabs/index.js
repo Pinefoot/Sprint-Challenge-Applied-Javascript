c86ce55d59c17c0c5256c7e8601c0a7d505251ef
Pinefoot <jameslcarpino@gmail.com>
2020-03-13 08:52:16 -0700

    trending topics is displaying correct information, but not sure of how it is supposed to fully look.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then (response =>{
+        console.log(response);
+        let allTabs = Tabs(response.data);
+        allTopics.append(allTabs);
+        // console.log('allTabs');
+    })
+    .catch(error =>{
+        console.log('Something is wrong!', error);
+    })
+
+
+//tab component function
+function Tabs(data){
+    //element
+    const navContainer = document.createElement('div');
+    //classes
+    navContainer.classList.add('tab');
+    //text content
+    navContainer.textContent = data.topics;
+    //appends
+    
+    //return it
+    return navContainer;
+}
+const allTopics = document.querySelector('.topics');
+
+  allTopics.append(Tabs());
